c241eb3ff05bd89e8f294c1d08562298a18c3687
AashutoshPathakShorthillsAI <aashutosh@shorthills.ai>
2024-06-07 17:47:34 +0530

    added models

--- AIStudioEnterprise/users/models.py
+++ AIStudioEnterprise/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+class StorageBucket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    base_url = models.URLField(blank=True)  
+    description = models.TextField(blank=True)  
+    order_by = models.CharField(max_length=100, blank=True)  
+
+class StorageCredential(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    storage = models.ForeignKey(StorageBucket, on_delete=models.CASCADE)
+
+    provider = models.CharField(max_length=50) 
+    access_key_id = models.CharField(max_length=200, blank=True)  
+    secret_access_key = models.CharField(max_length=200, blank=True) 
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
